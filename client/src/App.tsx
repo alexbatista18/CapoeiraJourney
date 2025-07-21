45c098b2ba3d012a1ada07be2b3736485a5ef791
alexbatista18 <dkalexbatista@gmail.com>
2025-07-21 03:28:37 -0300

    Fix GitHub Pages routing with SPA support and base path configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,10 +6,12 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "./pages/home";
 import NotFound from "@/pages/not-found";
 
-function Router() {
+function AppRouter() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/CapoeiraJourney" component={Home} />
+      <Route path="/CapoeiraJourney/" component={Home} />
       <Route component={NotFound} />
     </Switch>
   );
@@ -20,7 +22,9 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <Router base="/CapoeiraJourney">
+          <AppRouter />
+        </Router>
       </TooltipProvider>
     </QueryClientProvider>
   );
